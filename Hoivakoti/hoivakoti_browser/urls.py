daf930cb3a5a59509d9698a6e74e2721e21539a9
ttumppi <80556749+ttumppi@users.noreply.github.com>
2022-12-02 12:25:53 +0200

    Add files via upload

---
+++ Hoivakoti/hoivakoti_browser/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'hoivakoti'
+urlpatterns = [
+    #Home page
+    path('', views.homepage, name='homepage'),
+    path('asukkaat/', views.asukkaat, name='asukkaat'),
+    path('sopimus/', views.sopimus, name='sopimus'),
+    path('tilaajat/', views.tilaajat, name='tilaajat'),
+    path('palvelut/', views.palvelut, name='palvelut'),
+    path('tyontekijat/', views.tyontekijat, name='tyontekijat'),
+    path('open_sopimus/<int:sopimus_id>/', views.open_sopimus, name='open_sopimus')
+]
